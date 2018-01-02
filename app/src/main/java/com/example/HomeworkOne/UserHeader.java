1654efe326993d07c3d4ca66bbc6587fba157368
kafca <951268027@qq.com>
2018-01-02 20:28:14 +0800

    resize user's header

--- app/src/main/java/com/example/HomeworkOne/UserHeader.java
+++ app/src/main/java/com/example/HomeworkOne/UserHeader.java
@@ -93,15 +93,16 @@ public class UserHeader extends Activity implements InitView{
 
         imagePicker = ImagePicker.getInstance();
         imagePicker.setImageLoader(new PicassoImageLoader());   //设置图片加载器
+        imagePicker.setMultiMode(false);
         imagePicker.setShowCamera(true);  //显示拍照按钮
         imagePicker.setCrop(true);        //允许裁剪（单选才有效）
         imagePicker.setSaveRectangle(true); //是否按矩形区域保存
         imagePicker.setSelectLimit(1);    //选中数量限制
         imagePicker.setStyle(CropImageView.Style.RECTANGLE);  //裁剪框的形状
-        imagePicker.setFocusWidth(800);   //裁剪框的宽度。单位像素（圆形自动取宽高最小值）
-        imagePicker.setFocusHeight(800);  //裁剪框的高度。单位像素（圆形自动取宽高最小值）
-        imagePicker.setOutPutX(1000);//保存文件的宽度。单位像素
-        imagePicker.setOutPutY(1000);//保存文件的高度。单位像素
+        imagePicker.setFocusWidth(500);   //裁剪框的宽度。单位像素（圆形自动取宽高最小值）
+        imagePicker.setFocusHeight(500);  //裁剪框的高度。单位像素（圆形自动取宽高最小值）
+        imagePicker.setOutPutX(500);//保存文件的宽度。单位像素
+        imagePicker.setOutPutY(500);//保存文件的高度。单位像素
     }
 
     @Override
